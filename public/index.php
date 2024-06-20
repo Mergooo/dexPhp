427351fb810ea60e56a714a5570e00c2562b2612
Mergooo <Mergoo@protonmail.com>
2024-06-20 22:02:48 +0200

    update
    added some comments

--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
 <?php
+// Display all error reports
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// Load Composer's autoloader
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
+// Set up routing using FastRoute
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $route) {
     $route->addRoute('GET', '/dexPhp/', 'App\Controller\HomeController::index');
     $route->addRoute('GET', '/dexPhp/pokemon', 'App\Controller\PokemonController::index');
@@ -14,28 +17,32 @@ $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $rout
     $route->addRoute('GET', '/dexPhp/trainer/{id}/edit', 'App\Controller\TrainerController::edit'); // 編集フォームの表示
     $route->addRoute('POST', '/dexPhp/trainer/{id}/update', 'App\Controller\TrainerController::update'); // フォーム送信の処理
 });
+// Get the HTTP method and URI from the request
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 
-
+// Remove query string from URI if present
 if (false !== $pos = strpos($uri, '?')) {
     $uri = substr($uri, 0, $pos);
 }
 $uri = rawurldecode($uri);
 
-
+// Dispatch the request to the appropriate route handler
 $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 
-
+// Handle the route match result
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
+        // No matching route was found
         echo "404 Not Found";
         break;
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         $allowedMethods = $routeInfo[1];
+        // Method is not allowed for the matching route
         echo "405 Method Not Allowed";
         break;
     case FastRoute\Dispatcher::FOUND:
+        // Route was found, dispatch to the handler
         $handler = $routeInfo[1];
         $vars = $routeInfo[2];
         list($class, $method) = explode('::', $handler, 2);
--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -8,17 +8,22 @@ class PokemonController extends BaseController
 {
     private PokemonModel $model;
 
+    // Constructor to initialize the PokemonModel
     public function __construct()
     {
         $this->model = new PokemonModel();
     }
 
+    // Method to display the list of all Pokemon
     public function index()
     {
         $pokemon = $this->model->findAll();
 
+        // Load the view and pass the Pokemon data
         parent::loadView('index', 'pokemon', ['pokemon' => $pokemon]);
     }
+
+    
     # nächstes:
     # create zeigt formular für create an (CRUD), dass man neuen trainer hinzufügt
     # store ist dafür zuständig dass sachen in die daten in die db geschrieben werden
